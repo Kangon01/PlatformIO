9738f0d19870a9833da4b7d486f07320426d06a2
Noah Schmitz <151875810+Kangon01@users.noreply.github.com>
2025-06-04 11:55:38 +0200

    Arduino

---
+++ ESP-01_Relaisteuerung/ESP-01_Relaisteuerung.ino
@@ -0,0 +1,65 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid     = "KabelBox-9B90";
+const char* password = "13311814531144239571";
+
+IPAddress ip(192,168,0,4);
+IPAddress gateway(192,168,0,1);
+IPAddress subnet(255,255,255,0);
+
+#define RELAY_PIN 0
+
+ESP8266WebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+
+  // Mit WLAN verbinden
+  WiFi.config(ip, gateway, subnet);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Verbinde mit dem WLAN");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("IP-Adresse: ");
+  Serial.println(WiFi.localIP());
+
+  // HTTP Endpunkte
+  server.on("/on", [](){
+    digitalWrite(RELAY_PIN, HIGH);
+    server.sendHeader("Location", "/");
+    server.send(303);
+    Serial.println("Relais EIN");
+  });
+
+  server.on("/off", [](){
+    digitalWrite(RELAY_PIN, LOW);
+    server.sendHeader("Location", "/");
+    server.send(303);
+    Serial.println("Relais AUS");
+  });
+
+  server.on("/", [](){
+    String html = "<h1>Relais-Steuerung</h1>";
+    html += "<p>State: ";
+    html += (digitalRead(RELAY_PIN) ? "EIN" : "AUS");
+    html += "</p>";
+    html += "<a href=\"/on\"><button>EIN</button></a> ";
+    html += "<a href=\"/off\"><button>AUS</button></a>";
+    server.send(200, "text/html", html);
+  });
+
+  server.begin();
+  Serial.println("HTTP-Server gestartet");
+}
+
+void loop() {
+  server.handleClient();
+}
---
+++ NTP-Tester/NTP-Tester.ino
@@ -0,0 +1,115 @@
+#include <ESP8266WiFi.h>
+
+#include <time.h>
+ 
+// WLAN-Daten hier eintragen
+
+const char* ssid = "KabelBox-9B90";
+
+const char* password = "13311814531144239571";
+ 
+// Liste der zu testenden NTP-Server
+
+const char* ntpServers[] = {
+
+  "pool.ntp.org",
+
+  "de.pool.ntp.org",
+
+  "time.google.com",
+
+  "time.windows.com",
+
+  "ptbtime1.ptb.de"
+
+};
+ 
+const int ntpServerCount = sizeof(ntpServers) / sizeof(ntpServers[0]);
+ 
+void setup() {
+
+  Serial.begin(115200);
+
+  delay(1000);
+
+  Serial.println();
+
+  Serial.println("Starte NTP Test...");
+ 
+  WiFi.begin(ssid, password);
+
+  Serial.print("Verbinde mit WLAN");
+
+  while (WiFi.status() != WL_CONNECTED) {
+
+    delay(500);
+
+    Serial.print(".");
+
+  }
+
+  Serial.println(" verbunden!");
+
+}
+ 
+bool getNtpTime(const char* ntpServer) {
+
+  configTime(1 * 3600, 1 * 3600, ntpServer); // MEZ/CEST Sommerzeit optional anpassen
+
+  Serial.printf("Teste NTP Server: %s ... ", ntpServer);
+ 
+  // Warte auf Zeit-Sync max 5 Sekunden
+
+  int tries = 0;
+
+  time_t now = time(nullptr);
+
+  while (now < 8 * 3600 * 2 && tries < 10) {  // 8 Stunden als "ungültige Zeit" Grenze
+
+    delay(500);
+
+    Serial.print(".");
+
+    now = time(nullptr);
+
+    tries++;
+
+  }
+ 
+  if (now < 8 * 3600 * 2) {
+
+    Serial.println(" Fehler: Keine Antwort (Timeout)");
+
+    return false;
+
+  } else {
+
+    struct tm timeinfo;
+
+    localtime_r(&now, &timeinfo);
+
+    Serial.printf(" Erfolg: %02d:%02d:%02d\n", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+
+    return true;
+
+  }
+
+}
+ 
+void loop() {
+
+  for (int i = 0; i < ntpServerCount; i++) {
+
+    getNtpTime(ntpServers[i]);
+
+    delay(2000); // kurze Pause zwischen den Tests
+
+  }
+
+  Serial.println("Alle Server getestet, nächster Test in 60 Sekunden...\n");
+
+  delay(60000);
+
+}
+
+ 
